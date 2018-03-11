c770955efeebac2af4def7b76bb6aeb32a8d2dea
Justin Wood <woody@apple.com>
2018-03-10 21:30:26 -0800

    Updating for local

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const health = require('homeautomation-js-lib/health.js')
 const repeat = require('repeat')
 const request = require('request')
 
-var loginToken = null
-var mainSite = null
-
 require('homeautomation-js-lib/mqtt_helpers.js')
 
-  // Config
+const gatewayIP = process.env.CONTROLLER_IP
+const serialNumber = process.env.CONTROLLER_SERIAL_NUMBER
+
+// Config
 var topic_prefix = process.env.TOPIC_PREFIX
 
 if (_.isNil(topic_prefix)) {
@@ -43,41 +43,170 @@ var disconnectedEvent = function() {
     health.unhealthyEvent()
 }
 
+// Gateway Functions
+ar authToken = null
+
+function powerwallURL(path) {
+    return 'http://' + gatewayIP + '/' + path
+}
+
+function authenticate() {
+    const formData = {
+        force_sm_off: false,
+        password: 'S' + serialNumber,
+        username: '',
+    }
+    
+    const body = JSON.stringify(formData)
+    logging.info('auth body: ' + body)
+
+    request.post({url:powerwallURL('api/login/Basic'), 
+        body: body,   
+        headers: 
+            {
+                'Content-Type' : 'application/json'
+            },
+        }, 
+        function(err,httpResponse,body){ 
+            const responseJSON = JSON.parse(body)
+            if ( _.isNil(err) ) {
+                authToken = responseJSON.token
+                logging.info(' auth token: ' + authToken)
+            } else {
+                logging.error('error authenticate response body: ' + JSON.stringify(responseJSON))                
+            }
+            doGet(false, 'api/sitemaster/run')
+        }
+    )
+}
+
+function doGet(authenticate, url, callback) {
+    if ( authenticate ) {
+        request.get({url: powerwallURL(url), json:true}, 
+            function(err,httpResponse,responseBody) {
+                if ( !_.isNil(callback) ) {
+                    callback(err, httpResponse, responseBody)
+                }
+        }).auth(null, null, true, authToken)
+    } else {
+        request.get({url: powerwallURL(url), json:true}, 
+            function(err,httpResponse,responseBody) {
+                if ( !_.isNil(err)) {
+                    logging.info('       get err: ' + err)
+                    logging.info('  httpresponse: ' + httpResponse)
+                    logging.info('          body: ' + responseBody)    
+                }
+                if ( !_.isNil(callback) ) {
+                    callback(err, httpResponse, responseBody)
+                }
+        })
+    }
+}
+
+function doCommit() {
+    doGet(true, 'api/config/completed', function(err,httpResponse,responseBody){
+        logging.info('commit response body: ' + JSON.stringify(responseBody))
+
+    })
+}
+function doQuery() {
+    doGet(false, 'api/system_status/soe', function(err,httpResponse,response){
+        logging.debug('soe response body: ' + JSON.stringify(response))
+        if ( _.isNil(err ) ) {
+            const percent = response.percentage
+            health.healthyEvent()
+
+            client.smartPublish(topic_prefix + '/reserve/battery/percent', percent, mqttOptions)
+        }
+    })
+
+    doGet(false, 'api/meters/aggregates', function(err,httpResponse,response){
+        logging.debug('aggregate response body: ' + JSON.stringify(response))
+        // const default_real_mode = siteInfo.default_real_mode
+        
+        if ( _.isNil(err ) ) {
+            health.healthyEvent()
+            const solar_power = response.solar.instant_power
+            const grid_power = response.site.instant_power
+            const battery_power = response.battery.instant_power
+            const load_power = response.load.instant_power
+
+            client.smartPublish(topic_prefix + '/stats/solar_generation', solar_power.toFixed(2).toString(), mqttOptions)
+            client.smartPublish(topic_prefix + '/stats/grid_usage', grid_power.toFixed(2).toString(), mqttOptions)
+            client.smartPublish(topic_prefix + '/stats/battery_usage', battery_power.toFixed(2).toString(), mqttOptions)
+            client.smartPublish(topic_prefix + '/stats/home_load', load_power.toFixed(2).toString(), mqttOptions)
+            client.smartPublish(topic_prefix + '/stats/grid_active', grid_power > 50, mqttOptions)
+        }
+    })
+}
+
+function setMode(batteryMode) {
+    if ( _.isNil(authToken) )  {
+        authenticate()
+        logging.error('cannot set mode, not authenticated')
+        return
+    }
+    const formData = {
+        'mode': '' + batteryMode
+    }
+    
+    const url = powerwallURL('api/operation')
+
+    logging.info(' posting: ' + JSON.stringify(formData))
+    request.post(
+        { 
+            url: url, 
+            body: JSON.stringify(formData),   
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+        }, function(err,httpResponse,responseBody) { 
+            logging.info('response body: ' + responseBody)
+            doCommit()
+        }
+    ).auth(null, null, true, authToken)
+}
+
+function setReservePercent(percent) {
+    if ( _.isNil(authToken) )  {
+        authenticate()
+        logging.error('cannot set mode, not authenticated')
+        return
+    }
+    const formData = {
+        'backup_reserve_percent': percent
+    }
+    
+    const url = powerwallURL('api/operation')
+
+    logging.info(' posting: ' + JSON.stringify(formData))
+    request.post(
+        { 
+            url: url, 
+            body: JSON.stringify(formData),   
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+        }, function(err,httpResponse,responseBody) { 
+            logging.info('response body: ' + responseBody)
+            doCommit()
+        }
+    ).auth(null, null, true, authToken)
+}
+
+
 // Setup MQTT
 const client = mqtt.setupClient(connectedEvent, disconnectedEvent)
 
-
 client.on('message', (topic, message) => {
-    if ( _.isNil(loginToken)) return
-    if ( _.isNil(mainSite)) return
-
     logging.info(' ' + topic + ':' + message)
-    var options = { authToken: loginToken, siteID: mainSite }
 
     if ( topic.toString().includes(reserveTopicSuffix)) {
-        tjs.setSiteReservePercent(options, Number(message), function(setReserveError, siteReserveResult) {
-            if ( !_.isNil(setReserveError)) {
-                logging.error(JSON.stringify(setReserveError))
-            }
-            logging.debug('siteReserveResult: ' + JSON.stringify(siteReserveResult))
-
-        })
-
+        setReservePercent(message)                        
     } else if ( topic.toString().includes(modeTopicSuffix)) {
-        tjs.setSiteMode(options, message.toString(), function(setSiteModeError, siteModeResult) {
-            if ( !_.isNil(setSiteModeError)) {
-                logging.error(JSON.stringify(setSiteModeError))
-            }
-            logging.debug('siteModeResult: ' + JSON.stringify(siteModeResult))
-        })
-                        
+        setMode(message.toString())
     }
 })
-
-var tjs = require('teslajs')
- 
-var username = process.env.TESLA_USERNAME
-var password = process.env.TESLA_PASSWORD
 
 var polling = false
 const startPolling = function() {
@@ -87,155 +216,9 @@ const startPolling = function() {
     repeat(doPoll).every(5, 's').start.in(5, 'sec')
 }
 
-function pollSiteInfo() {
-    if ( _.isNil(loginToken)) return
-    if ( _.isNil(mainSite)) return
-    
-    logging.info('Polling site info')
-
-    var options = { authToken: loginToken, siteID: mainSite }
-    
-    tjs.siteInfo(options, function(siteInfoError, siteInfo) {
-        if ( !_.isNil(siteInfoError)) {
-            logging.error(JSON.stringify(siteInfoError))
-            health.unhealthyEvent()
-            return
-        }
-        
-        logging.debug('siteInfo: ' + JSON.stringify(siteInfo))
-        if ( !_.isNil(siteInfo) ) {
-            const backup_reserve_percent = siteInfo.backup_reserve_percent
-            const default_real_mode = siteInfo.default_real_mode
-            const solar = siteInfo.components.solar
-            const grid = siteInfo.components.grid
-            const battery = siteInfo.components.battery
-            const batteryCount = siteInfo.battery_count
-            const version = siteInfo.version
-            
-            logging.info('=   Solar: ' + solar)
-            logging.info('=   Grid: ' + grid)
-            logging.info('=   Battery: ' + battery)
-            logging.info('=   Battery Count: ' + batteryCount)
-            logging.info('=   Version: ' + version)
-            logging.info('=   Current Battery Mode: ' + default_real_mode)
-            logging.info('=   Current Battery Reserve %: ' + backup_reserve_percent)
-
-            client.smartPublish(topic_prefix + '/reserve/mode', default_real_mode.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/reserve/percent', backup_reserve_percent.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/system/version', version.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/system/battery_count', batteryCount.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/system/has_batteries', battery.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/system/has_solar', solar.toString(), mqttOptions)
-            client.smartPublish(topic_prefix + '/system/has_grid', grid.toString(), mqttOptions)
-
-            health.healthyEvent()
-        }
-    })
-}
-
-function pollLiveStatus() {
-    if ( _.isNil(loginToken)) return
-    if ( _.isNil(mainSite)) return
-    
-    logging.info('Polling live status')
-
-    var options = { authToken: loginToken, siteID: mainSite }
-
-    tjs.siteStatus(options, function(siteStatusError, siteStatus) {
-        if ( !_.isNil(siteStatusError)) {
-            logging.error(JSON.stringify(siteStatusError))
-            health.unhealthyEvent()
-            return
-        }
-
-        logging.debug('siteStatus: ' + JSON.stringify(siteStatus))
-        const solar_power = siteStatus.solar_power
-        const grid_status = siteStatus.grid_status
-        const grid_power = siteStatus.grid_power
-        const battery_power = siteStatus.battery_power
-        const energy_left = siteStatus.energy_left
-        const total_pack_energy = siteStatus.total_pack_energy
-        const load_power = siteStatus.load_power
-        logging.info('=   Solar Generation: ' + solar_power)
-        logging.info('=   Grid Usage: ' + grid_power)
-        logging.info('=   Battery Usage: ' + battery_power)
-        logging.info('=   Total Home Load: ' + load_power)
-
-        logging.info('=   Grid Status: ' + grid_status)
-        logging.info('=   Total Battery Capacity: ' + total_pack_energy)
-        logging.info('=   Battery Remaining: ' + energy_left)
-        logging.info('=   Battery %: ' + ((energy_left / total_pack_energy) * 100))
-        
-        client.smartPublish(topic_prefix + '/stats/solar_generation', solar_power.toFixed(2).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/stats/grid_usage', grid_power.toFixed(2).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/stats/battery_usage', battery_power.toFixed(2).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/stats/home_load', load_power.toFixed(2).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/stats/grid_active', grid_status.toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/reserve/battery/percent', ((energy_left / total_pack_energy) * 100).toFixed(1).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/reserve/battery/remaining', energy_left.toFixed(0).toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/reserve/battery/capacity', total_pack_energy.toString(), mqttOptions)
-        client.smartPublish(topic_prefix + '/reserve/battery/charging', (battery_power < 0) ? '1': '0', mqttOptions)
-        client.smartPublish(topic_prefix + '/reserve/battery/discharging', (battery_power > 0) ? '1': '0', mqttOptions)
-
-        health.healthyEvent()
-    })
-}
-
 function doPoll() {
-    if ( _.isNil(loginToken)) return
-    if ( _.isNil(mainSite)) return
-
-    pollLiveStatus()
-    pollSiteInfo()
+    doQuery()
 }
 
-const handleLogin = function(token) {
-    var options = { authToken: token }
-
-    tjs.products(options, function(err, products) {
-        if ( !_.isNil(err)) {
-            logging.error(JSON.stringify(err))
-        }
-        logging.debug('products: ' + JSON.stringify(products))
-        if ( _.isNil(products)) {
-            logging.error('empty products!')
-            return
-        }
-        const energy_site_id = products.energy_site_id
-        const site_name = products.site_name
-        const gateway_id = products.gateway_id
-
-        mainSite = energy_site_id
-        loginToken = token
-
-        logging.info('===========================')
-        logging.info('=  ' + site_name)
-        logging.info('=  ')
-        logging.info('=  Site ID: ' + energy_site_id)
-        logging.info('=  Gateway ID: ' + gateway_id)
-        
-        startPolling()
-    })
-}
-
-function doLogin() {
-    tjs.login(username, password, function(err, result) {
-        if (result.error) {
-            logging.error(JSON.stringify(result.error))
-            process.exit(1)
-        }
-
-        var token = JSON.stringify(result.authToken).toString().replace(/"/gi,'')
-        logging.debug('auth response: ' + JSON.stringify(result))
-        
-        if (token) {
-            logging.info('Login Succesful - token: ' + token)
-            handleLogin(token)
-        } else {
-            logging.error('no token responded with')
-        }
-    })
-}
-
-
-doLogin()
+authenticate()
+startPolling()
