ad86816f295534568273f6f12eab5d18386f554a
Justin Wood <woody@apple.com>
2018-03-13 14:33:23 -0700

    Cleaning this up

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const mqtt = require('mqtt')
 const _ = require('lodash')
  const logging = require('homeautomation-js-lib/logging.js')
-const health = require('homeautomation-js-lib/health.js')
-const repeat = require('repeat')
-const request = require('request')
+ const health = require('homeautomation-js-lib/health.js')
+ 
+ const tesla = require('./lib/tesla.js')
 
 require('homeautomation-js-lib/mqtt_helpers.js')
-
-const gatewayIP = process.env.CONTROLLER_IP
-const serialNumber = process.env.CONTROLLER_SERIAL_NUMBER
 
 // Config
 var topic_prefix = process.env.TOPIC_PREFIX
@@ -47,160 +44,6 @@ var disconnectedEvent = function() {
     health.unhealthyEvent()
 }
 
-// Gateway Functions
-var authToken = null
-
-function powerwallURL(path) {
-    return 'http://' + gatewayIP + '/' + path
-}
-
-function authenticate() {
-    const formData = {
-        force_sm_off: false,
-        password: 'S' + serialNumber,
-        username: '',
-    }
-    
-    const body = JSON.stringify(formData)
-    logging.info('auth body: ' + body)
-
-    request.post({url:powerwallURL('api/login/Basic'), 
-        body: body,   
-        headers: 
-            {
-                'Content-Type' : 'application/json'
-            },
-        }, 
-        function(err,httpResponse,body){ 
-            const responseJSON = JSON.parse(body)
-            if ( _.isNil(err) ) {
-                authToken = responseJSON.token
-                logging.info(' auth token: ' + authToken)
-            } else {
-                logging.error('error authenticate response body: ' + JSON.stringify(responseJSON))                
-            }
-            doGet(false, 'api/sitemaster/run')
-        }
-    )
-}
-
-function doGet(authenticate, url, callback) {
-    if ( authenticate ) {
-        request.get({url: powerwallURL(url), json:true}, 
-            function(err,httpResponse,responseBody) {
-                if ( !_.isNil(callback) ) {
-                    callback(err, httpResponse, responseBody)
-                }
-        }).auth(null, null, true, authToken)
-    } else {
-        request.get({url: powerwallURL(url), json:true}, 
-            function(err,httpResponse,responseBody) {
-                if ( !_.isNil(err)) {
-                    logging.info('       get err: ' + err)
-                    logging.info('  httpresponse: ' + httpResponse)
-                    logging.info('          body: ' + responseBody)    
-                }
-                if ( !_.isNil(callback) ) {
-                    callback(err, httpResponse, responseBody)
-                }
-        })
-    }
-}
-
-function doCommit() {
-    doGet(true, 'api/config/completed', function(err,httpResponse,responseBody){
-        logging.info('commit response body: ' + JSON.stringify(responseBody))
-
-    })
-}
-function doQuery() {
-    doGet(false, 'api/system_status/soe', function(err,httpResponse,response){
-        logging.info('soe response body: ' + JSON.stringify(response))
-        if ( _.isNil(err) && !_.isNil(response) ) {
-            const percent = Number(response.percentage).toFixed(2)
-            health.healthyEvent()
-
-            client.publish(topic_prefix + '/reserve/battery/percent', '' + percent, mqttOptions)
-        }
-    })
-
-    doGet(false, 'api/meters/aggregates', function(err,httpResponse,response){
-        logging.info('aggregate response body: ' + JSON.stringify(response))
-        // const default_real_mode = siteInfo.default_real_mode
-        
-        if ( _.isNil(err) && !_.isNil(response) ) {
-            health.healthyEvent()
-            const solar_power = !_.isNil(response.solar) ? response.solar.instant_power : 0
-            const grid_power = !_.isNil(response.site) ? response.site.instant_power : 0 
-            const battery_power = !_.isNil(response.battery) ? response.battery.instant_power : 0
-            const load_power = !_.isNil(response.load) ? response.load.instant_power : 0
-
-            client.publish(topic_prefix + '/stats/solar_generation', solar_power.toFixed(2).toString(), mqttOptions)
-            client.publish(topic_prefix + '/stats/grid_usage', grid_power.toFixed(2).toString(), mqttOptions)
-            client.publish(topic_prefix + '/stats/battery_usage', battery_power.toFixed(2).toString(), mqttOptions)
-            client.publish(topic_prefix + '/stats/home_load', load_power.toFixed(2).toString(), mqttOptions)
-            client.publish(topic_prefix + '/stats/grid_active', '' + (grid_power > 50 ? '0' : '1'), mqttOptions)
-        } else {
-            health.unhealthyEvent()
-        }
-    })
-}
-
-function setMode(batteryMode) {
-    if ( _.isNil(authToken) )  {
-        logging.error('cannot set mode, not authenticated')
-        return
-    }
-    const formData = {
-        'mode': '' + batteryMode,
-        'backup_reserve_percent': 70
-    }
-    
-    const url = powerwallURL('api/operation')
-
-    logging.info(' posting: ' + JSON.stringify(formData))
-    request.post(
-        { 
-            url: url, 
-            body: JSON.stringify(formData),   
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-        }, function(err,httpResponse,responseBody) { 
-            logging.info('response body: ' + responseBody)
-            doCommit()
-        }
-    ).auth(null, null, true, authToken)
-}
-
-function setReservePercent(percent) {
-    if ( _.isNil(authToken) )  {
-        logging.error('cannot set mode, not authenticated')
-        return
-    }
-    const formData = {
-        'mode': 'self_consumption',
-        'backup_reserve_percent': Number(percent)
-    }
-    
-    const url = powerwallURL('api/operation')
-
-    logging.info(' posting: ' + JSON.stringify(formData))
-    request.post(
-        { 
-            url: url, 
-            body: JSON.stringify(formData),   
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-        }, function(err,httpResponse,responseBody) { 
-            logging.info('response body: ' + responseBody)
-            doCommit()
-        }
-    ).auth(null, null, true, authToken)
-}
-
-
 // Setup MQTT
 logging.info('connecting to MQTT host')
 client = mqtt.setupClient(connectedEvent, disconnectedEvent)
@@ -209,21 +52,67 @@ client.on('message', (topic, message) => {
     logging.info(' ' + topic + ':' + message)
 
     if ( topic.toString().includes(reserveTopicSuffix)) {
-        setReservePercent(message)                        
+        tesla.setReservePercent(message)                        
     } else if ( topic.toString().includes(modeTopicSuffix)) {
-        setMode(message.toString())
+        tesla.setMode(message.toString())
     }
 })
 
-const startPolling = function() {    
-    logging.info('starting poll')
-    repeat(doPoll).every(5, 's').start.in(2, 'sec')
-}
+tesla.on('soe-updated', (result) => {
+    if (_.isNil(result)) {
+        logging.error(' soe-updated failed')
+        health.unhealthyEvent()
+        return
+    }
 
-function doPoll() {
-    doQuery()
-}
+    client.publish(topic_prefix + '/reserve/battery/percent', '' + result.toFixed(2), mqttOptions)
+    health.healthyEvent()
+})
 
+tesla.on('solar-updated', (result) => {
+    if (_.isNil(result)) {
+        logging.error(' soe-updated failed')
+        health.unhealthyEvent()
+        return
+    }
 
-authenticate()
-startPolling()
+    client.publish(topic_prefix + '/stats/solar_generation', result.toFixed(2).toString(), mqttOptions)
+    health.healthyEvent()
+})
+
+tesla.on('grid-updated', (result) => {
+    if (_.isNil(result)) {
+        logging.error(' grid-updated failed')
+        health.unhealthyEvent()
+        return
+    }
+
+    client.publish(topic_prefix + '/stats/grid_usage', result.toFixed(2).toString(), mqttOptions)
+    client.publish(topic_prefix + '/stats/grid_active', '' + (result > 50 ? '0' : '1'), mqttOptions)
+    health.healthyEvent()
+})
+
+tesla.on('battery-updated', (result) => {
+    if (_.isNil(result)) {
+        logging.error(' battery-updated failed')
+        health.unhealthyEvent()
+        return
+    }
+
+    client.publish(topic_prefix + '/stats/battery_usage', result.toFixed(2).toString(), mqttOptions)
+    health.healthyEvent()
+})
+
+tesla.on('load-updated', (result) => {
+    if (_.isNil(result)) {
+        logging.error(' load-updated failed')
+        health.unhealthyEvent()
+        return
+    }
+
+    client.publish(topic_prefix + '/stats/home_load', result.toFixed(2).toString(), mqttOptions)
+    health.healthyEvent()
+})
+
+tesla.startPolling()
+
